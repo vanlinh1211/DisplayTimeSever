e263e0c0b35fc4726438e4c8c72e7c2105537e0d
vanlinh12111 <vanlang1211@gmail.com>
2019-04-05 15:11:51 +0700

    commit

---
+++ echo_sever_time.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>hiển thị thời gian</title>
+</head>
+<body>
+<?php
+echo date('Y-M-d h:i:s');
+?>
+</body>
+</html>
